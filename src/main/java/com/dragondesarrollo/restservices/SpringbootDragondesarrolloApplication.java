9b9c0f142d6294f7e2a016846b6b6fb859c38af3
Israel Vences <israelvencesmondragon@gmail.com>
2022-05-19 10:43:38 -0500

    First commit

---
+++ src/main/java/com/dragondesarrollo/restservices/SpringbootDragondesarrolloApplication.java
@@ -0,0 +1,13 @@
+package com.dragondesarrollo.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDragondesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDragondesarrolloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dragondesarrollo/restservices/SpringbootDragondesarrolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragondesarrollo.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDragondesarrolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
